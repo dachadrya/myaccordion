0718e73a5e0da8f35711eaa01e1b726632842079
Daya Acharya <daya.acharya@gmail.comgit config --global user.email>
2022-10-19 18:02:27 -0400

    use chakraUi Accordion comonent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from '@chakra-ui/react';
 
-function App() {
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <Accordion>
+  <AccordionItem>
+    <h2>
+      <AccordionButton>Note 1</AccordionButton>
+    </h2>
+    <AccordionPanel>Design kit from cms is great but it's accordion component is not working. 
+      Accordion component of chara UI is working</AccordionPanel>
+  </AccordionItem>
+
+  <AccordionItem>
+    <h2>
+      <AccordionButton>Note 2</AccordionButton>
+    </h2>
+    <AccordionPanel> There are many more component need to make for pretter web application.</AccordionPanel>
+  </AccordionItem>
+</Accordion> 
+        
+        
     </div>
   );
 }
